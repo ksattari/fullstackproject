d66ea338cb22229d95a0134add6b0f285590e730
Khashayar <ksattari@yahoo.com>
2023-03-27 15:44:49 -0700

    daily push

--- Final Project/customer_service/src/main/java/com/fullstackproject/restaurant/customer_service/controller/OrderController.java
+++ Final Project/customer_service/src/main/java/com/fullstackproject/restaurant/customer_service/controller/OrderController.java
@@ -1,11 +1,13 @@
 package com.fullstackproject.restaurant.customer_service.controller;
 
 import com.fullstackproject.restaurant.customer_service.model.MenuItem;
+import com.fullstackproject.restaurant.customer_service.model.Order;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -29,5 +31,12 @@ public class OrderController implements WebMvcConfigurer {
         model.addAttribute("theMenu",menu);
 
         return "order";
+    }
+
+   @PostMapping("/processOrder")
+    public String processOrder(Order order, WebRequest w, Model model){
+
+        log.info(order.toString());
+        return "confirmation";
     }
 }
