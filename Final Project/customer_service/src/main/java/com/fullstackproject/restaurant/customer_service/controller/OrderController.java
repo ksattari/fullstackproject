a60d98c74f58e6d693b1d601f8f04023d226c930
Khashayar <ksattari@yahoo.com>
2023-03-27 13:09:47 -0700

    daily push

--- Final Project/customer_service/src/main/java/com/fullstackproject/restaurant/customer_service/controller/OrderController.java
+++ Final Project/customer_service/src/main/java/com/fullstackproject/restaurant/customer_service/controller/OrderController.java
@@ -1,19 +1,33 @@
 package com.fullstackproject.restaurant.customer_service.controller;
 
+import com.fullstackproject.restaurant.customer_service.model.MenuItem;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 
 @Controller
 @Slf4j
-public class OrderController {
+public class OrderController implements WebMvcConfigurer {
+
+    @Autowired
+    List<MenuItem> menu;
 
     @GetMapping("/")
-    public String orderPage(Model model){
+    public String orderPage(Model model, WebRequest w){
         log.info("INSIDE orderPage METHOD");
-        model.addAttribute("theDate",new java.util.Date());
+        model.addAttribute("theDate", new SimpleDateFormat("MM/dd/yyyy")
+                .format(new Date()));
+        model.addAttribute("theMenu",menu);
+
         return "order";
     }
 }
