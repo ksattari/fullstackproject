c100a4bf4badd4e580ddcf57f682a79682345358
Khashayar <ksattari@yahoo.com>
2023-03-31 10:16:14 -0700

    daily push

--- admin_panel/restaurant_admin/src/app/app.module.ts
+++ admin_panel/restaurant_admin/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MenuListComponent } from './menu-list/menu-list.component';
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppComponent,
     OrderListComponent,
     OrderDialogComponent,
+    MenuListComponent,
 
   ],
   imports: [
