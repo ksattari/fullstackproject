3984f2f1a5a29f58ce642be3f16e102ca6dfe1d7
Khashayar <ksattari@yahoo.com>
2023-03-30 17:37:50 -0700

    daily push

--- admin_panel/restaurant_admin/src/app/order-list/order-list.component.ts
+++ admin_panel/restaurant_admin/src/app/order-list/order-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { Order } from '../model/order';
 import { OrderService } from '../service/order.service';
 
